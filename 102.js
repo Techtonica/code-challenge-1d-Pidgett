108d2752c8ffc12e33ab655391009a9d905f19e8
TPL8_2021H1 <taela.luccia@gmail.com>
2021-01-26 08:28:16 -0800

    Solve and push 102.js to correct repository.

--- 102.js
+++ 102.js
@@ -13,7 +13,19 @@
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Comparison_Operators#Relational_operators
 
 function largestInt(a,b){
-  return // your code here
-}
+  if (a > b){
+    console.log(a, "is the largest integer.");
+    return a;
+  } else if(a < b){
+    console.log(b, "is the largest integer.");
+    return b;
+  } else{
+    console.log(a, "is equal to", b+".");
+    return a;
+  }
+};
+largestInt(114,87);
+largestInt(65,41);
+largestInt(10,10);
 
 module.exports = largestInt;
